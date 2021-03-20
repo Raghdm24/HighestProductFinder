d7271bb963ead65139ddfa0fc091c6023e85199a
Raghdm24 <Raghdmadhun@gmail.com>
2021-03-20 04:37:12 -0700

    Updated code to use max()

--- HighestProductFinger.py
+++ HighestProductFinger.py
@@ -1,7 +1,6 @@
 ################################################## Import ##################################################
 import math
 #################################################Functions#################################################
-
 def CanProductRight(col):
     return True if col + Neighbors -1 < MatrixSize and col >= 0 else False
 
@@ -111,15 +110,11 @@ def mulAll():
             index = GetHighestIndex(cellList)
             direction = switcher(index)
 
-            allList.append([cellList[index], direction])
-
-
+            allList.append([cellList[index], direction , (c,r)])
     return allList
-
 
 def GetHighestIndex(list):
     return list.index(max(list))
-
 
 def switcher(x):
     return {
@@ -132,7 +127,6 @@ def switcher(x):
         6: 'leftUp',
         7: 'leftDown',
     }[x]
-
 
 def RunAllTests():
     if TestCanProductRight()==False:
@@ -182,23 +176,23 @@ def RunAllTests():
 MatrixSize = 20
 Neighbors = 4
 
-someWierdInput = "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\n"
+someWierdInput = "99 00 99 99 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\n"
 someWierdInput +="49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00\n"
 someWierdInput +="81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65\n"
 someWierdInput +="52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91\n"
-someWierdInput +="22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80\n"
-someWierdInput +="24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50\n"
-someWierdInput +="32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70\n"
-someWierdInput +="67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21\n"
+someWierdInput +="22 31 16 71 51 67 63 89 41 92 36 54 22 99 40 28 66 33 13 80\n"
+someWierdInput +="24 47 32 60 99 03 45 02 44 75 33 53 00 36 84 20 35 17 12 50\n"
+someWierdInput +="32 98 81 28 64 23 67 10 26 38 40 99 59 54 70 66 18 38 64 70\n"
+someWierdInput +="67 26 20 99 99 00 99 20 95 63 99 39 63 08 40 91 66 49 94 21\n"
 someWierdInput +="24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72\n"
 someWierdInput +="21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95\n"
 someWierdInput +="78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92\n"
 someWierdInput +="16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57\n"
 someWierdInput +="86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58\n"
 someWierdInput +="19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 99 89 55 40\n"
-someWierdInput +="04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 99 27 98 66\n"
+someWierdInput +="04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 00 27 98 66\n"
 someWierdInput +="88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 99 93 53 69\n"
-someWierdInput +="04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 99 62 76 36\n"
+someWierdInput +="04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 23 62 76 36\n"
 someWierdInput +="20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16\n"
 someWierdInput +="20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54\n"
 someWierdInput +="01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
@@ -448,14 +442,28 @@ for row in rows:
 #print(Testret())
 #print(RunAllTests())
 
-list = mulAll()
-list.sort(key=lambda x: x[0])  #the highest list is: [70600674, 'leftDown'], [70600674, 'rightUp']
+list= mulAll()
+newList = max(list)
+print(newList)
+Cc= newList[2][0]
+Rr= newList[2][1]
+Dd= newList[1]
 
-print(list)
+def direction(Dd):
+    return {
+        'right': PointRight(Cc,Rr),
+        'left' : PointLeft(Cc,Rr),
+        'up'   : PointUp(Cc,Rr),
+        'down' : PointDown(Cc,Rr),
+        'rightUp': PointDiagonalRightUP(Cc,Rr),
+        'rightDown': PointDiagonalRightDown(Cc,Rr),
+        'leftUp': PointDiagonalLeftUp(Cc,Rr),
+        'leftDown': PointDiagonalLeftDown(Cc,Rr),
+    }[Dd]
 
-#index = list.index([70600674, 'rightUp'])
-#print('The index of the highest product in list is: ',index)  #303 , #246
-
+FourValues = direction(Dd)
+index = list.index(newList)
+print("\n" + "The highest value within the matrix is =" + str(newList[0]),"\n" + "The index in the matrix is :" + str(index), "\n" + "Column and Row in order" + str(newList[2]) ,"\n" + "The direction : " + str(newList[1]) ,"\n" + "The 4 values of the highest product is: " + str(FourValues) )
 
 
 '''
@@ -479,6 +487,7 @@ print(list)
 20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+
 '''
 
 
